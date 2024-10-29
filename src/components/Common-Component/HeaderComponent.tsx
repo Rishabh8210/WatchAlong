d9a9ae44243ff0fc277a1dd6d9b37a280ad942af
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-10-29 21:50:34 +0530

    Fact: Fixes some css for mobile design

--- src/components/Common-Component/HeaderComponent.tsx
+++ src/components/Common-Component/HeaderComponent.tsx
@@ -5,9 +5,9 @@ const HeaderComponent = () => {
     const [isStyle, setStyle] = useState(false);
     return (
         <div className='h-fit w-full flex items-center justify-between overflow-hidden flex-shrink-0 flex-grow-0 border-b-2 border-zinc-800'>
-            <div className='h-10 w-1/2 relative overflow-hidden transition-all' onMouseEnter={() => setStyle(true)} onMouseOut={() => setStyle(false)}>
-                <p className={isStyle ? 'text-3xl absolute -top-10 transition-all font-mono font-semibold pointer-events-none' : 'text-3xl absolute  top-0 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong</p>
-                <p className={isStyle ? 'text-4xl absolute top-0 transition-all uppercase font-mono font-semibold pointer-events-none' : 'text-3xl absolute top-10 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong</p>
+            <div className='h-10 w-2/3 relative overflow-hidden transition-all' onMouseEnter={() => setStyle(true)} onMouseOut={() => setStyle(false)}>
+                <p className={isStyle ? 'text-3xl absolute -top-10 transition-all font-mono font-semibold pointer-events-none' : 'text-3xl absolute  top-0 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong.</p>
+                <p className={isStyle ? 'text-3xl absolute top-0 transition-all uppercase font-mono font-semibold pointer-events-none' : 'text-3xl absolute top-10 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong!</p>
             </div>
             <img className='h-[70px] cursor-pointer hover:scale-110 hover:rotate-12 transition-all' src='./Logo.png' alt='#Logo' />
         </div>
--- src/components/HomePage-Component/HomePageMain.tsx
+++ src/components/HomePage-Component/HomePageMain.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const HomePageMain = () => {
     return (
         <div className='h-[60vh] w-full  flex flex-col items-center justify-center'>
-            <div className='h-4/6 w-5/6 border-2 rounded-lg bg-zinc-950 border-zinc-700 flex flex-col items-center justify-center gap-10'>
+            <div className='h-4/6 w-full border-2 rounded-lg bg-zinc-950 border-zinc-700 flex flex-col items-center justify-center gap-10'>
                 <HomePageTitleSlidingBar />
                 <Link className='h-fit w-fit px-4 py-2 text-lg font-semibold transition-all rounded-lg border-2 border-zinc-700 bg-zinc-900 hover:bg-zinc-950' href={'/rooms'}>Create Room</Link>
             </div>
