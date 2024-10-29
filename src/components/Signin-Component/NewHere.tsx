550ed2748f5076b5363df65e7756c1b1a3438163
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-10-29 13:28:48 +0530

    Fact : Little changes in stylesheet

--- src/components/Signin-Component/NewHere.tsx
+++ src/components/Signin-Component/NewHere.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const NewHere = () => {
     return (
-        <div className='h-fit w-full flex justify-center rounded-md p-5 border-2 items-center bg-black border-zinc-700'>
+        <div className='h-fit w-full md:w-2/5 flex justify-center rounded-md p-5 border-2 items-center bg-black border-zinc-700'>
             <div className="h-fit w-full flex gap-1 justify-center items-center">
                 <p>New to WatchAlong?</p>
-                <Link className="text-blue-600 " href={'/signup'}>Register</Link>
+                <Link className="text-blue-700 font-semibold " href={'/signup'}>Register</Link>
             </div>
         </div>
     )
--- src/components/Signin-Component/SigninMain.tsx
+++ src/components/Signin-Component/SigninMain.tsx
@@ -1,15 +1,14 @@
 "use client"
-import Link from "next/link"
 import { useState } from "react"
 
 const Signin = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     return (
-        <div className='min-h-[40vh] w-full p-5 rounded-md border-2 flex justify-center items-center bg-zinc-900 border-zinc-700'>
+        <div className='min-h-[40vh] w-full md:w-2/5 p-5 rounded-md border-2 flex justify-center items-center bg-zinc-900 border-zinc-700'>
             <form className="h-fit w-full flex flex-col justify-center items-center gap-5">
                 <div className="h-fit w-full flex justify-center items-center flex-col">
-                    <p className="px-4 w-full font-semibold">Email</p>
+                    <p className="px-4 md:px-6 w-full font-semibold">Email</p>
                     <input
                         type="email"
                         className="h-11 w-11/12 text-gray-300 px-3 rounded-lg bg-black bg-opacity-45 border-2 border-zinc-600 outline-none focus:border-blue-600"
@@ -21,7 +20,7 @@ const Signin = () => {
                 </div>
 
                 <div className="h-fit w-full flex justify-center items-center flex-col">
-                    <p className="px-4 w-full font-semibold">Password</p>
+                    <p className="px-4 md:px-6 w-full font-semibold">Password</p>
                     <input
                         type="password"
                         className="h-11 w-11/12 px-3 text-gray-300 rounded-lg bg-black bg-opacity-45 border-2 border-zinc-600 outline-none focus:border-blue-600"
@@ -33,7 +32,7 @@ const Signin = () => {
                 </div>
 
                 <div className="h-fit w-full flex justify-center items-center flex-col">
-                    <button className="bg-green-600 h-fit p-1.5 w-5/12 rounded-lg text-lg font-semibold" onClick={() => alert(`${email + password}`)}>SignIn</button>
+                    <button className="bg-green-600 h-fit p-1.5 w-5/12 rounded-lg text-lg font-semibold hover:text-green-600 hover:bg-transparent hover:border-2 border-zinc-700">SignIn</button>
                 </div>
             </form>
         </div>
