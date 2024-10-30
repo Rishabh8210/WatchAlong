596cf1a35c652a17c1893835ad55c57804fa78f4
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-10-30 18:37:19 +0530

    Commit : Addded dropdown menu feature

--- src/components/Common-Component/HeaderComponent.tsx
+++ src/components/Common-Component/HeaderComponent.tsx
@@ -1,15 +1,18 @@
 "use client"
 import React, { useState } from 'react'
+import DropDownMenu from './DropDownMenu';
 
 const HeaderComponent = () => {
     const [isStyle, setStyle] = useState(false);
+    const [openDropDown, setDropDown] = useState(false);
     return (
         <div className='h-fit w-full flex items-center justify-between overflow-hidden flex-shrink-0 flex-grow-0 border-b-2 border-zinc-800'>
             <div className='h-10 w-2/3 relative overflow-hidden transition-all' onMouseEnter={() => setStyle(true)} onMouseOut={() => setStyle(false)}>
                 <p className={isStyle ? 'text-3xl absolute -top-10 transition-all font-mono font-semibold pointer-events-none' : 'text-3xl absolute  top-0 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong.</p>
                 <p className={isStyle ? 'text-3xl absolute top-0 transition-all uppercase font-mono font-semibold pointer-events-none' : 'text-3xl absolute top-10 transition-all font-mono font-semibold pointer-events-none'}>WatchAlong!</p>
             </div>
-            <img className='h-[60px] cursor-pointer hover:scale-110 hover:rotate-12 transition-all' src='./Logo.png' alt='#Logo' />
+            <img className='h-[60px] cursor-pointer hover:scale-110 hover:rotate-12 transition-all' src='./Logo.png' alt='#Logo' onClick={() => setDropDown(!openDropDown)} />
+            {openDropDown && <DropDownMenu />}
         </div>
     )
 }
--- src/components/Testonomial-Component/TestonomialCard.tsx
+++ src/components/Testonomial-Component/TestonomialCard.tsx
@@ -4,7 +4,7 @@ const TestonomialCard = () => {
   return (
     <div className='min-h-[40vh] w-full py-5 flex justify-between flex-col gap-10 flex-shrink-0'>
         <div className='h-fit w-full'>
-            <p>I just wanted to say that Watch2Gether is absolutely wonderful, me and my girlfriend are in a long-distance relationship and we use it all the time.</p>
+            <p>I just wanted to say that WatchAlong is absolutely wonderful, me and my girlfriend are in a long-distance relationship and we use it all the time.</p>
         </div>
         <div className='h-fit w-full flex gap-3 pt-2 items-center border-t-2 border-zinc-500'>
             <img className='invert h-5' src="./user.png" alt="#User" />
