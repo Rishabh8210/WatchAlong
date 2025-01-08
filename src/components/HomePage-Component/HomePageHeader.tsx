441d6308abc9ea480565aa65107c3868c173345a
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-01-09 01:46:45 +0530

    Fact: Fixes some css bugs in Homepage Header section

--- src/components/HomePage-Component/HomePageHeader.tsx
+++ src/components/HomePage-Component/HomePageHeader.tsx
@@ -15,16 +15,16 @@ const HomePageHeader = () => {
                 <p className={isStyle ? 'text-3xl absolute -top-10 transition-all font-mono font-semibold pointer-events-none dark:text-white' : 'text-3xl absolute  top-0 transition-all font-mono font-semibold pointer-events-none dark:text-white'}>WatchAlong.</p>
                 <p className={isStyle ? 'text-3xl absolute top-0 transition-all uppercase font-mono font-semibold pointer-events-none dark:text-white' : 'text-3xl absolute top-10 transition-all font-mono font-semibold pointer-events-none dark:text-white'}>WatchAlong!</p>
             </div>
-            <div className='h-fit w-full flex items-center justify-end gap-5'>
+            <div className='hidden h-fit w-full sm:flex items-center justify-end gap-5'>
                 <Link href={'/rooms'} className="hidden sm:flex h-10 w-44 justify-center items-center flex-col">
                     <button className="bg-yellow-600 h-fit uppercase p-1.5 w-full rounded-md text-xl font-mono font-semibold hover:text-yellow-600 hover:bg-transparent hover:border-2 border-zinc-700">Create Room</button>
                 </Link>
                 <Link href={'/signin'} className="hidden md:flex h-10 w-28 justify-center items-center flex-col">
                     <button className="bg-green-600 h-fit p-1.5 w-full rounded-md text-lg font-mono font-semibold hover:text-green-600 hover:bg-transparent hover:border-2 border-zinc-700">SignIn</button>
                 </Link>
-                <img className='h-[60px] sm:hidden cursor-pointer hover:scale-110 hover:rotate-12 transition-all' src='./Logo.png' alt='#Logo' onClick={() => setDropDown(!openDropDown)} />
-                {openDropDown && <DropDownMenu />}
             </div>
+            <img className='h-[60px] sm:hidden cursor-pointer hover:scale-110 hover:rotate-12 transition-all' src='./Logo.png' alt='#Logo' onClick={() => setDropDown(!openDropDown)} />
+            {openDropDown && <DropDownMenu />}
         </div>
     )
 }
