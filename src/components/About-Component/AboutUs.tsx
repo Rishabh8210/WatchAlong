b9fc6a50409ad79f47f139040300696a5ac85cf0
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-01-11 23:52:46 +0530

    Commit: Refactor the UI design

--- src/components/About-Component/AboutUs.tsx
+++ src/components/About-Component/AboutUs.tsx
@@ -4,9 +4,11 @@ import AboutMain from './AboutMain'
 
 const AboutUs = () => {
   return (
-    <div className='min-h-[60vh] py-5 flex flex-col items-center w-full'>
-        <AboutHeader />
-        <AboutMain />
+    <div className='min-h-[60vh] w-full flex flex-col justify-around items-center'>
+      <div className='h-full w-full sm:w-10/12 md:w-9/12 lg:max-w-3xl py-5 flex flex-col items-center'>
+          <AboutHeader />
+          <AboutMain />
+      </div>
     </div>
   )
 }
--- src/components/StepsToBeFollowed/StepsToBeFollowed.tsx
+++ src/components/StepsToBeFollowed/StepsToBeFollowed.tsx
@@ -3,16 +3,18 @@ import Header from './Header'
 
 const StepsToBeFollowed = () => {
   return (
-    <div className='h-[60vh] w-full flex flex-col justify-around items-center p-5 gap-2 '>
-        <Header />
-        <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white p-3 rounded-xl hover:text-white hover:bg-zinc-900'>
-            <p className='text-xl font-semibold  pointer-events-none'>1. Create a room</p>
-        </div>
-        <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white hover:text-white p-3 rounded-xl hover:bg-zinc-900'>
-            <p className='text-xl font-semibold pointer-events-none'>2. Share the code</p>
-        </div>
-        <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white hover:text-white p-3 rounded-xl hover:bg-zinc-900'>
-            <p className='text-xl font-semibold pointer-events-none'>3. Watch Together</p>
+    <div className='h-[60vh] w-full flex flex-col justify-around items-center'>
+        <div className='h-full w-full sm:w-10/12 md:w-9/12 lg:max-w-3xl flex flex-col justify-around items-center p-5 gap-2 '>
+            <Header />
+            <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white p-3 rounded-xl hover:text-white hover:bg-zinc-900'>
+                <p className='text-xl font-semibold  pointer-events-none'>1. Create a room</p>
+            </div>
+            <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white hover:text-white p-3 rounded-xl hover:bg-zinc-900'>
+                <p className='text-xl font-semibold pointer-events-none'>2. Share the code</p>
+            </div>
+            <div className='h-fit w-full border-b-2 border-zinc-800 dark:border-white hover:text-white p-3 rounded-xl hover:bg-zinc-900'>
+                <p className='text-xl font-semibold pointer-events-none'>3. Watch Together</p>
+            </div>
         </div>
     </div>
   )
